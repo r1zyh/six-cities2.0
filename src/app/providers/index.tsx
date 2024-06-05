f591877a0f07aefc0f2f247e8ec2ed3fa4bead61
r1zyh <kirill.ananiev2012@yandex.ru>
2024-06-05 23:31:00 +0300

    Начнет разбивку Offer page на компоненты

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,7 +1,8 @@
 import { Main } from '../../pages/main/ui';
+import { Offer } from '../../pages/offer/ui';
 
 function Providers() {
-  return <Main />;
+  return <Offer/>;
 }
 
 export default Providers;
