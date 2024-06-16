8beb9c14eacca42bfcc1f2acb947aab0961b1745
r1zyh <kirill.ananiev2012@yandex.ru>
2024-06-16 23:40:29 +0300

    Настроит Vite config, исправит ошибку с алиасами

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,20 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@widgets': path.resolve(__dirname, './src/widgets'),
+      '@entities': path.resolve(__dirname, './src/entities')
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
