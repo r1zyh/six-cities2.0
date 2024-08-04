22a000213e740d054ff7506c668823d94abb4f85
Kirill Ananyev <richeeq@gmail.com>
2024-08-04 20:44:21 +0300

    Добавит favorite-empty

--- src/pages/favorites/ui/favorites.tsx
+++ src/pages/favorites/ui/favorites.tsx
@@ -1,20 +1,39 @@
 import { Header } from '@widgets/header/ui';
 import { Footer } from '@widgets/footer/ui';
 import { FavoritesList } from '@features/favorites-list/ui';
+import { useAppSelector } from '@shared/lib/hooks/useSelector/useSelector';
+import { getFavoriteOffersCount } from '@shared/store/offer-process';
 
 export function Favorites(): JSX.Element {
+  const favCount = useAppSelector(getFavoriteOffersCount);
   return (
     <div className="page">
       <Header />
-
-      <main className="page__main page__main--favorites">
-        <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <FavoritesList />
-          </section>
-        </div>
-      </main>
+      {favCount === 0 ? (
+        <main className="page__main page__main--favorites page__main--favorites-empty">
+          <div className="page__favorites-container container">
+            <section className="favorites favorites--empty">
+              <h1 className="visually-hidden">Favorites (empty)</h1>
+              <div className="favorites__status-wrapper">
+                <b className="favorites__status">Nothing yet saved.</b>
+                <p className="favorites__status-description">
+                  Save properties to narrow down search or plan your future
+                  trips.
+                </p>
+              </div>
+            </section>
+          </div>
+        </main>
+      ) : (
+        <main className="page__main page__main--favorites">
+          <div className="page__favorites-container container">
+            <section className="favorites">
+              <h1 className="favorites__title">Saved listing</h1>
+              <FavoritesList />
+            </section>
+          </div>
+        </main>
+      )}
       <Footer />
     </div>
   );
--- src/widgets/footer/ui/footer.tsx
+++ src/widgets/footer/ui/footer.tsx
@@ -1,7 +1,10 @@
+import { AppRoute } from '@shared/const';
+import { Link } from 'react-router-dom';
+
 export function Footer(): JSX.Element {
   return (
     <footer className="footer container">
-      <a className="footer__logo-link" href="main.html">
+      <Link className="footer__logo-link" to={AppRoute.Main}>
         <img
           className="footer__logo"
           src="img/logo.svg"
@@ -9,7 +12,7 @@ export function Footer(): JSX.Element {
           width="64"
           height="33"
         />
-      </a>
+      </Link>
     </footer>
   );
 }
